bfb8a51d28cb1b645a7c4a62de66efd4fd518a03
Gacho2021 <87105994+Gacho2021@users.noreply.github.com>
2022-01-28 14:59:33 -0800

    Update multiple-object.js

--- multiple-object.js
+++ multiple-object.js
@@ -4,7 +4,7 @@ let msg = '<h2>hotel 1</h2>name: dakota<p>total rooms: 70';
 msg += '<h2>hotel 2</h2>name: silver cloud<p>total rooms: 50';
  
 // set a variable for selectots
-var el = document.getElementById('info');
+let el = document.getElementById('info');
  //inserted variable into the objcts
 el.innerHTML = msg;
  
